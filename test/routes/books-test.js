446a897b4a4cf33e37f69eeb8e64f209e27952f2
Jiyuan Mao <1078120230@qq.com>
2018-11-05 17:59:04 +0000

    Get books by category endpoint tested

--- test/routes/books-test.js
+++ test/routes/books-test.js
@@ -133,6 +133,45 @@ describe('Books', function (){
             });
         });
 
+        describe('GET /books/bcategory/:category',  () => {
+            it('should return books according to category you search for', function(done) {
+                chai.request(server)
+                    .get('/books/bcategory/Computing Science')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.a('array');
+                        expect(res.body.length).to.equal(1);
+                        let result = _.map(res.body, (book) => {
+                            return {
+                                name: book.name,
+                                author:book.author,
+                                publisher:book.publisher,
+                                category:book.category,
+                            }
+                        });
+                        expect(result).to.include( { name: 'Building Web Sites All-in-One Desk Reference For Dummies',
+                            author: 'Doug Sahlin',
+                            publisher:'John Wiley & Sons',
+                            category:'Computing Science',
+                        } );
+                        done();
+                    });
+            });
+            it('should return a 404 and a message for invalid book category', function(done) {
+                chai.request(server)
+                    .get('/books/bcategory/abc')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(404);
+                        expect(res.body).to.have.property('message','Category NOT Found!' ) ;
+                        done();
+                    });
+            });
+            after(function(done) {
+                Book.collection.drop();
+                done();
+            });
+        });
+
 
     });
 
