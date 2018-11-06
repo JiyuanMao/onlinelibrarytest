c9185c5526bcb58c278e60a4cefd0c4caa2ccd45
Jiyuan Mao <1078120230@qq.com>
2018-11-06 00:53:29 +0000

    Get comments by fuzzysearch endpoint tested

--- test/routes/comments-test.js
+++ test/routes/comments-test.js
@@ -62,5 +62,43 @@ describe('Comments', function () {
                 });
             });
         });
+        describe('GET /comments/search/:bookname',  () => {
+            describe('when fuzzy search book name is correct', function(done) {
+                it('should return one or more comments you fuzzy search for', function (done) {
+                    chai.request(server)
+                        .get('/comments/search/build')
+                        .end(function (err, res) {
+                            expect(res).to.have.status(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).to.equal(1);
+                            let result = _.map(res.body, (comment) => {
+                                return {
+                                    text: comment.text,
+                                    username: comment.username,
+                                    bookname: comment.bookname,
+                                }
+                            });
+                            expect(result).to.include({
+                                text: "it is very useful",
+                                bookname: "Building Web Sites All-in-One Desk Reference For Dummies",
+                                username: "john"
+                            });
+
+                            done();
+                        });
+                });
+            });
+            describe('when fuzzy search book name is wrong', function(done) {
+                it('should return a 404 and a message for invalid keyword', function (done) {
+                    chai.request(server)
+                        .get('/comments/search/abc')
+                        .end(function (err, res) {
+                            expect(res).to.have.status(404);
+                            expect(res.body).to.have.property('message', 'Comment NOT Found!');
+                            done();
+                        });
+                });
+            });
+        });
     });
 })
